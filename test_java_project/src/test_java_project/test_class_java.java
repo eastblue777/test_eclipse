406d2fcfe072e780610a75a9a1718bb182536e3d
Jiung Jeon <eastblue77@gmail.com>
2019-04-30 11:24:38 +0900

    added print line

--- test_java_project/src/test_java_project/test_class_java.java
+++ test_java_project/src/test_java_project/test_class_java.java
@@ -2,6 +2,7 @@ package test_java_project;
 
 public class test_class_java {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World~! hotfix!!");
+		
 	}
 }
